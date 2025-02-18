b045c4fc0f538079350cd147385dd2443f692389
berengerraoul <berenger.raoul@gmail.com>
2025-02-18 17:40:50 +0100

    Update Rplot2.R

--- Rplot2.R
+++ Rplot2.R
@@ -6,3 +6,5 @@ plot(donnees$datetime, donnees$Global_active_power, type="l",
 pos_labels <- as.POSIXct(c("2007-02-01 00:00:00", "2007-02-02 00:00:00", "2007-02-03 00:00:00"))
 
 axis(1, at=pos_labels, labels=c("Thu", "Fri", "Sat"))
+
+png("plot2.png", width = 480, height = 480)
