23fe204e7352473346b060d6631d980e0a043dd4
berengerraoul <berenger.raoul@gmail.com>
2025-02-18 17:42:33 +0100

    Update Rplot4.R

--- Rplot4.R
+++ Rplot4.R
@@ -29,5 +29,6 @@ plot(donnees$datetime, donnees$Global_reactive_power, type="l", col="black",
      xaxt="n")  # Désactiver l'axe X par défaut
 pos_labels <- as.POSIXct(c("2007-02-01 00:00:00", "2007-02-02 00:00:00", "2007-02-03 00:00:00"))
 
-png("plot4.png", width = 480, height = 480)
 axis(1, at=pos_labels, labels=c("Thu", "Fri", "Sat"))
+
+png("plot4.png", width = 480, height = 480)
